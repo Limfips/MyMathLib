5e9d7df4c11c8552f71587e78373aca52eedb586
Limfips Limfips <limfips7@gmail.com>
2019-04-15 08:39:36 +0400

    v0.2 Начало

--- MyMathLib/Class1.cs
+++ MyMathLib/Class1.cs
@@ -1,10 +1,17 @@
-﻿using System.Runtime.Remoting.Messaging;
+﻿using System;
 
 namespace MyMathLib
 {
     public class Math
     {
-        public Math(int x1, int x2, int dx, int eps, int n)
+        private readonly double _dx; //Шаг
+        private readonly double _eps; //Точность
+
+        private readonly double _x1; //Интервал от
+        private readonly double _x2; //Bynthdfk до
+        private double _n = 1;
+
+        public Math(double x1, double x2, double dx, double eps)
         {
             _x1 = x1;
             _x2 = x2;
@@ -12,21 +19,19 @@ namespace MyMathLib
             _eps = eps;
         }
 
-        private int _x1;    //Интервал от
-        private int _x2;    //Bynthdfk до
-        private int _dx;    //Шаг
-        private int _eps;   //Точность
-        private int _n = 1;
-
         public double Calc()
         {
             double result = 0;
+            double interimResult;
             var x = _x1;
-            while (x>=-1 && x<1)
+            while (x >= -1 && x < 1 || x >= _x2)
             {
-                
+                interimResult = System.Math.Pow(x,_n)/_n;
+                result += interimResult - interimResult % _eps;
+                x += _dx;
+                _n = System.Math.Pow(_n,2);
             }
-            
+
             return -result;
         }
     }
